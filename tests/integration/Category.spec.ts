c3b0287112ffa94ae982346c6e6b02863e22aa5d
domiebett <dbett49@gmail.com>
2020-04-01 00:43:53 +0300

    chore(eureka): move start method to EurekaService

---
+++ tests/integration/Category.spec.ts
@@ -0,0 +1,9 @@
+import * as request from 'supertest';
+import { Express } from '../../src/middleware/config/Express';
+import * as jest from 'jest';
+
+const app = new Express().app;
+
+describe('Category', () => {
+
+});
